fe54864c830e4c5e6df9407508cded5d8894b569
just_for_the_soul (Лига А) <just_for_the_soul@mail.ru>
2021-04-21 23:10:56 +0200

    разбивает на камни

---
+++ src/utils/render.js
@@ -0,0 +1,23 @@
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+const renderElement = (container, component, place = 'beforeend') => {
+  switch (place) {
+    case 'prepend':
+      container.prepend(component);
+      break;
+    case 'afterend':
+      container.after(component);
+      break;
+    case 'beforeend':
+      container.append(component);
+      break;
+  }
+};
+
+export {createElement, renderElement};
